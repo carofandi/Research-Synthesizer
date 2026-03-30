fe096d80edb91776cd1941d6b3cd29e519e4951a
Carola Fandino <carolafandino@Carolas-MacBook-Pro.local>
2026-03-30 12:56:48 -0300

    fix port and error logging

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+require('dotenv').config(require('dotenv').config({ path: '.env' });
+  );
 const express = require('express');
 const cors = require('cors');
 const { fetchYouTube, fetchHN, fetchPolymarket, fetchWeb } = require('./fetchers');
